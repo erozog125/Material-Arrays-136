fd84048698eb542c2c8a448bda1108566f07e67a
AlissonJE <alissonjaramillo62@gmail.com>
2026-05-08 12:39:29 -0500

    creacion carpeta scripts e implementacion array para guardado de los prodcutos ya existentes

---
+++ alisson-market/Styles/Scripts/Product.js
@@ -0,0 +1,12 @@
+ const product = [
+
+    // Creacion Array con elementos ya existentes
+
+    {id: 1, name: "Sabrina Carpenter Vinyl", precio: 50, Imagen: "vinyl1.jpg"},
+    {id: 2, name: "Limp Biskit Vinyl", precio: 60, Imagen: "vinyl2.jpg"},
+    {id:3, name: "Radiohead Vinyl", precio: 50, Imagen:"vinyl3.jpg"},
+    {id:4, name: "Lady Gaga Vinyl", precio: 60, Imagen:"vinyl4.jpg"},
+    {id:5, name: "Bon jovi", precio: 70, Imagen:"vinyl5.jpg"},
+    {id:6, name: "Bruno Mars", precio: 60, Imagen:"vinyl6.jpg"}
+
+]
