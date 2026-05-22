a56c3627f67809e0aea2bfcd6e5371dc0bf83d54
Mafe1324 <mariaf.huertasm@uqvirtual.edu.co>
2026-05-21 21:40:51 -0500

    Corregir rutas de imagen

--- market.js
+++ market.js
@@ -10,49 +10,49 @@ const productosDestacados = [
     id: 1,
     nombre: "Ramo de flores",
     precio: 35000,
-    imagen: "assets/ramo-flores-1.jpg",
+    imagen: "assets/ramo-flores-1.JPG",
     descripcion: "Ideal para complementar detalles románticos, familiares o fechas especiales."
   },
   {
     id: 2,
     nombre: "Ramo de flores especial",
     precio: 75000,
-    imagen: "assets/ramo-flores-2.jpg",
+    imagen: "assets/ramo-flores-2.png",
     descripcion: "Una opción más llamativa para hacer el regalo más elegante y significativo."
   },
   {
     id: 3,
     nombre: "Tabla de quesos",
     precio: 40000,
-    imagen: "assets/tabla-quesos-1.jpg",
+    imagen: "assets/tabla-quesos-1.jpeg",
     descripcion: "Complemento gastronómico para hacer el detalle más completo y especial."
   },
   {
     id: 4,
     nombre: "Charcutería",
     precio: 40000,
-    imagen: "assets/tabla-quesos-2.jpg",
+    imagen: "assets/tabla-quesos-2.jpeg",
     descripcion: "Una opción ideal para anchetas, celebraciones o desayunos más especiales."
   },
   {
     id: 5,
     nombre: "Domo variado grande",
     precio: 40000,
-    imagen: "assets/domo-grande-1.jpg",
+    imagen: "assets/domo-grande-1.png",
     descripcion: "Perfecto para agregar dulces, fresas, chocolates u otros productos disponibles."
   },
   {
     id: 6,
     nombre: "Domo decorado",
     precio: 40000,
-    imagen: "assets/domo-grande-2.jpg",
+    imagen: "assets/domo-grande-2.png",
     descripcion: "Un adicional bonito y llamativo para acompañar desayunos o anchetas."
   },
   {
     id: 7,
     nombre: "Parfait",
     precio: 18000,
-    imagen: "assets/parfait-1.jpg",
+    imagen: "assets/parfait.jpeg",
     descripcion: "Una opción fresca y dulce para complementar el desayuno sorpresa."
   }
 ];
